92c1319e33c3f8bd0b6880bfc55758c4e8231892
attilagyoriid <attila.gyori.id@gmail.com>
2024-01-05 18:12:11 +0100

    refactor

--- src/test/java/com/ag/java/algo/sorts/InsertionSortTest.java
+++ src/test/java/com/ag/java/algo/sorts/InsertionSortTest.java
@@ -14,12 +14,14 @@ class InsertionSortTest {
         Assertions.assertArrayEquals(new int[]{1,2,3,4,8,9}, array);
     }
 
+    @Test
     void sort_empty() {
         int[] array = new int[]{};
         InsertionSort.sort(array);
         Assertions.assertArrayEquals(new int[]{}, array);
     }
 
+    @Test
     void sort_one_item() {
         int[] array = new int[]{5};
         InsertionSort.sort(array);
