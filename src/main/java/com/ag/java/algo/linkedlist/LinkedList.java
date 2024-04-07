61a0f02478c25141765ae72a6d9cb13fd6d5e9e5
attilagyoriid <attila.gyori.id@gmail.com>
2024-04-07 14:39:07 +0200

    Linked list comment

--- src/main/java/com/ag/java/algo/linkedlist/LinkedList.java
+++ src/main/java/com/ag/java/algo/linkedlist/LinkedList.java
@@ -482,4 +482,96 @@ public class LinkedList {
         return true;
     }
 
+    /**
+     *
+     * @param startIndex
+     * @param endIndex
+     *
+     *
+     * Check if the head of the list is null.
+     *
+     * If the list is empty, there is nothing to reverse. Return immediately (do nothing).
+     *
+     * Create a dummyNode with value 0.
+     *
+     * This node serves as a placeholder to simplify operations involving the list head.
+     *
+     * Set the next pointer of dummyNode to point to the head of the list.
+     *
+     * Initialize a variable previousNode and set it to dummyNode.
+     *
+     * previousNode will track the node right before the segment we want to reverse.
+     *
+     * Move previousNode forward startIndex steps to position it just before the start of the segment to reverse.
+     *
+     * Use a loop with an index i going from 0 to startIndex.
+     *
+     * Set a currentNode variable to point to the next node after previousNode.
+     *
+     * currentNode will be the first node of the segment to be reversed.
+     *
+     * Now we're ready to reverse the segment.
+     *
+     * Perform the following steps endIndex - startIndex times:
+     *
+     * Set a nodeToMove variable to point to the next node after currentNode. This is the node
+     * we will "cut" from the segment and "paste" to the front of the segment.
+     *
+     * Update currentNode.next to skip over nodeToMove and point to the node right after nodeToMove.
+     *
+     * Set the next pointer of nodeToMove to point to the node at the front of the segment. This is the node that previousNode.next is pointing to.
+     *
+     * Update previousNode.next to point to nodeToMove. nodeToMove has now been moved to the front of the segment.
+     *
+     * After the loop, the segment between startIndex and endIndex is reversed.
+     *
+     * Update the head of the list to point to the actual first node (not dummyNode).
+     *
+     * Set head to point to dummyNode.next.
+     *
+     *
+     *
+     * Example in plain English:
+     *
+     * Imagine you have a deck of cards numbered from 1 to 10 in order. Now, I want you to reverse the cards between positions 3 and 7.
+     *
+     * Here's how you can do it:
+     *
+     * Find the 3rd Card: Locate the card at the 3rd position. Let's call this card the "previousNode card" and place it on the table.
+     *
+     * Identify the Starting Card: Look at the next card in line. Name this the "currentNode card." This card marks the beginning of the section to reverse.
+     *
+     * Prepare for the Magic Trick: Get ready to perform a magic trick on the next set of cards.
+     *
+     * Select Next Card: Focus on the "currentNode card." Pick up the card immediately after it, and call it the "nodeToMove card."
+     *
+     * Rearrange Cards: Move the "currentNode card" so that it sits behind the "nodeToMove card."
+     *
+     * Place the Card: Lay the "nodeToMove card" down on the table next to the "previousNode card." Now, the "nodeToMove card" has moved to the front.
+     *
+     * Repeat the Process: Continue this process until you reach the card at the 7th position. By the end, the cards between the 3rd and 7th positions should be in reverse order.
+     */
+    public void reverseBetween(int startIndex, int endIndex) {
+        if (head == null) return;
+
+        Node dummyNode = new Node(0);
+        dummyNode.next = head;
+        Node previousNode = dummyNode;
+
+        for (int i = 0; i < startIndex; i++) {
+            previousNode = previousNode.next;
+        }
+
+        Node currentNode = previousNode.next;
+
+        for (int i = 0; i < endIndex - startIndex; i++) {
+            Node nodeToMove = currentNode.next;
+            currentNode.next = nodeToMove.next;
+            nodeToMove.next = previousNode.next;
+            previousNode.next = nodeToMove;
+        }
+
+        head = dummyNode.next;
+    }
+
 }
